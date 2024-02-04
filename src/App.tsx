3875a3fa8b1dc512d8cddd22080b4759a5460a25
Joseph Maloch <jozefmaloch@gmail.com>
2024-02-04 22:14:07 +0800

    Fix warning.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Container, Grid, GridItem } from '@chakra-ui/react';
-import useAbstractProvider from './providers/AbstractProvider';
-import AppApi from './api/app';
-import CustomLayout from './components/layout/Layout';
 import CustomRouter from './CustomRouter';
 
 function App() {
