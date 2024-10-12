b96c1bd1069e6271f3478e5d04fe1446a0844d05
Joseph Maloch <jozefmaloch@gmail.com>
2024-10-12 19:51:50 +0800

    In ActivityCalendarPage add to the tooltip the todos, qaas and rest after a 1s delay on hover on a day box.

--- src/components/ActivityCalendar/ActivityCalendarPage.tsx
+++ src/components/ActivityCalendar/ActivityCalendarPage.tsx
@@ -1,23 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Box, Grid, VStack, Heading, Tooltip } from '@chakra-ui/react';
-import {
-  eachDayOfInterval,
-  eachMonthOfInterval,
-  format,
-  startOfYear,
-  endOfYear,
-  getYear,
-} from 'date-fns';
+import { eachDayOfInterval, format, startOfYear, endOfYear } from 'date-fns';
 import ActivityCalendarProvider from '../../providers/ActivityCalendarProvider';
+import GithubProvider from '../../providers/GithubProvider';
 
 interface Activity {
-  todos: number;
-  qaas: number;
-  blogs: number;
-  goals: number;
-  habits_trackers: number;
-  total: number;
+  todos?: number;
+  qaas?: number;
+  blogs?: number;
+  goals?: number;
+  habits_trackers?: number;
+  total?: number;
+  github?: number;
 }
+
 interface ActivityData {
   days: {
     [date: string]: Activity;
@@ -28,7 +24,7 @@ interface ActivityData {
 
 // Function to determine the color based on activity level
 const getDayColor = (activityCount: number, isToday: boolean): string => {
-  if (isToday) return 'blue.400'; // Highlight today in red
+  if (isToday) return 'blue.400'; // Highlight today in blue
   if (activityCount > 20) return 'green.600';
   if (activityCount > 10) return 'green.400';
   if (activityCount > 5) return 'green.200';
@@ -42,9 +38,14 @@ const ActivityCalendarPage: React.FC = () => {
     months: {},
     years: {},
   });
-  console.log('activityData: ', activityData);
+  const [contributions, setContributions] = useState<[] | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null); // State for the selected date
   const { getDailyActivityCountsData, getDailyActivityCounts } =
     ActivityCalendarProvider();
+
+  const { fetchGitHubContributionsData, fetchGitHubContributions } =
+    GithubProvider();
 
   useEffect(() => {
     getDailyActivityCounts();
@@ -61,11 +62,46 @@ const ActivityCalendarPage: React.FC = () => {
     }
   }, [getDailyActivityCountsData]);
 
-  // Generate all days and months of the current year
   const allDays = eachDayOfInterval({
     start: startOfYear(new Date()),
     end: endOfYear(new Date()),
   });
+
+  useEffect(() => {
+    if (getDailyActivityCountsData) {
+      fetchGitHubContributions();
+    }
+  }, [getDailyActivityCountsData]);
+
+  useEffect(() => {
+    console.log('fetchGitHubContributionsData: ', fetchGitHubContributionsData);
+    if (fetchGitHubContributionsData) {
+      setContributions(fetchGitHubContributionsData);
+    }
+  }, [fetchGitHubContributionsData]);
+
+  useEffect(() => {
+    if(contributions){
+    const newActivityData = { ...activityData };
+    contributions?.forEach((week: any) => {
+      week.contributionDays.forEach((day: any) => {
+        const formattedDate = day.date;
+
+        if (!newActivityData.days[formattedDate]) {
+          newActivityData.days[formattedDate] = {
+            github: 0,
+          };
+        }
+        newActivityData.days[formattedDate].github = day.contributionCount;
+      });
+      setActivityData((prevData) => ({
+        ...prevData,
+        ...newActivityData,
+      }));
+    });
+  }
+
+  }, [contributions]);
 
   const allMonths = eachMonthOfInterval({
     start: startOfYear(new Date()),
@@ -77,7 +113,24 @@ const ActivityCalendarPage: React.FC = () => {
     (_, i) => getYear(new Date()) - 9 + i,
   ); // Generate the last 10 years
 
-  const today = format(new Date(), 'yyyy-MM-dd'); // Get today's date in 'yyyy-MM-dd' format
+  const today = format(new Date(), 'yyyy-MM-dd');
+
+  const handleMouseEnter = (formattedDate: any) => {
+    console.log('handleMouseEnter');
+    timeoutRef.current = setTimeout(() => {
+      console.log('Setting selected date');
+      setSelectedDate(formattedDate);
+    }, 1000);
+  };
+
+  const handleMouseLeave = () => {
+    console.log('Removing timeout');
+    setSelectedDate(null);
+    if (timeoutRef.current !== null) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+  };
 
   return (
     <VStack spacing={8}>
@@ -92,12 +145,56 @@ const ActivityCalendarPage: React.FC = () => {
             : `${formattedDate} - ${activityCount} contributions`;
 
           return (
-            <Tooltip label={tooltipLabel} key={index} placement="top">
+            <Tooltip
+              label={
+                <>
+                  <Box>{tooltipLabel}</Box>
+                  {selectedDate ? (
+                    <VStack spacing={0} alignItems="flex-start">
+                      <Box>
+                        <strong>Todos:</strong>{' '}
+                        {activityData.days[selectedDate]?.todos}
+                      </Box>
+                      <Box>
+                        <strong>Qaas:</strong>{' '}
+                        {activityData.days[selectedDate]?.qaas}
+                      </Box>
+                      <Box>
+                        <strong>Blogs:</strong>{' '}
+                        {activityData.days[selectedDate]?.blogs}
+                      </Box>
+                      <Box>
+                        <strong>Goals:</strong>{' '}
+                        {activityData.days[selectedDate]?.goals}
+                      </Box>
+                      <Box>
+                        <strong>Habits:</strong>{' '}
+                        {activityData.days[selectedDate]?.habits_trackers}
+                      </Box>
+                      <Box>
+                        <strong>Total:</strong>{' '}
+                        {activityData.days[selectedDate]?.total}
+                      </Box>
+                      <Box>
+                        <strong>GitHub:</strong>{' '}
+                        {activityData.days[selectedDate]?.github}
+                      </Box>
+                    </VStack>
+                  ) : null}
+                </>
+              }
+              key={index}
+              placement="top"
+              hasArrow
+            >
               <Box
                 width="20px"
                 height="20px"
                 bg={getDayColor(activityCount, isToday)} // Check if it's today
                 borderRadius="md"
+                cursor="pointer" // Indicate that the box is clickable
+                onMouseEnter={() => handleMouseEnter(formattedDate)}
+                onMouseLeave={() => handleMouseLeave()}
               />
             </Tooltip>
           );
