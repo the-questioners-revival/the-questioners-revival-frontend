cef51351fc838935d49d09c4524a83e27fbba1d5
Joseph Maloch <jozefmaloch@gmail.com>
2024-12-11 02:59:57 +0800

    Remove editor from EditableItemDetails.

--- src/components/Category/EditableItemDetails.tsx
+++ src/components/Category/EditableItemDetails.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Input, Text, Button, Box, Textarea } from '@chakra-ui/react';
+import React from 'react';
 import { Todo, Qaa, Blog } from '../../types';
 import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Link from '@tiptap/extension-link';
-import HtmlEditor from '../HtmlEditor/HtmlEditor';
 import EditQaaForm from '../Qaa/EditQaaForm';
 import EditBlogForm from '../Blog/EditBlogForm';
 import EditTodoForm from '../Todo/EditTodoForm';
@@ -24,41 +22,6 @@ const EditableItemDetails: React.FC<EditableItemDetailsProps> = ({
   saveChanges,
   handleCancelClick,
 }) => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Link.configure({
-        openOnClick: false,
-        autolink: true,
-      }),
-    ],
-    editorProps: {
-      attributes: {
-        class: 'Editor',
-      },
-    },
-    content: '',
-  });
-
-  useEffect(() => {
-    if (editor) {
-      if (editedItem?.answer) {
-        editor.commands.setContent(editedItem.answer);
-      }
-      if (editedItem?.text) {
-        editor.commands.setContent(editedItem.text);
-      }
-    }
-  }, [editedItem, editor]);
-  console.log('selectedItem: ', selectedItem);
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    field: string,
-  ) => {
-    if (editedItem) {
-      setEditedItem({ ...editedItem, [field]: e.target.value });
-    }
-  };
 
   if (!selectedItem) return null;
 
